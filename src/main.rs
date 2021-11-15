dbad2df9f5d98b86b123fed05b3cc768a90430a6
Benoit Delemps <benoitdelemps@gmail.com>
2021-11-15 16:03:15 +0100

    Window is now persistent

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,23 @@
 extern crate glium;
 
 fn main() {
-    println!("Hello, world!");
+    use glium::glutin;
+
+    let events_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &events_loop).expect("display building failed");
+
+    events_loop.run(move |ev, _, control_flow| {
+        // Setting the event loop to 60 FPS fallback
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        if let glutin::event::Event::WindowEvent { event, .. } = ev {
+            if event == glutin::event::WindowEvent::CloseRequested {
+                *control_flow = glutin::event_loop::ControlFlow::Exit;
+            }
+        }
+    })
 }
