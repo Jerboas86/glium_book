88803b82f589151f6b488b97c1d1e20e31d218f5
Benoit Delemps <benoitdelemps@gmail.com>
2021-11-16 14:38:50 +0100

    add 2D triangle

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,53 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &events_loop).expect("display building failed");
 
-    let mut target = display.draw();
-    target.clear_color(0., 0., 1., 1.);
-    target.finish().unwrap();
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+
+    let vertex2 = Vertex {
+        position: [0.5, -0.25],
+    };
+
+    let vertex3 = Vertex {
+        position: [0., 0.5],
+    };
+
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+    #version 140
+
+    in vec2 position;
+
+    void main() {
+        gl_Position = vec4(position, 0.0, 1.0 );
+    }
+    "#;
+
+    let fragment_shader_src = r#"
+    #version 140
+
+    out vec4 color;
+    void main() {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+    "#;
+
+    let program =
+        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+            .unwrap();
 
     events_loop.run(move |ev, _, control_flow| {
         // Setting the event loop to 60 FPS fallback
@@ -26,5 +70,19 @@ fn main() {
                 *control_flow = glutin::event_loop::ControlFlow::Exit;
             }
         }
-    })
+
+        let mut target = display.draw();
+        target.clear_color(0., 0., 1., 1.);
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &glium::uniforms::EmptyUniforms,
+                &Default::default(),
+            )
+            .unwrap();
+
+        target.finish().unwrap();
+    });
 }
