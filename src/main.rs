13fdbb957dc225376a9d6004c5391e95df7365b9
Benoit Delemps <benoitdelemps@gmail.com>
2021-11-17 17:18:39 +0100

    red teapot

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use glium::Surface;
-use std::io::Cursor;
 
 #[macro_use]
 extern crate glium;
 extern crate image;
+
+mod teapot;
 
 fn main() {
     use glium::glutin;
@@ -13,71 +14,35 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &events_loop).expect("display building failed");
 
-    let image = image::load(
-        Cursor::new(&include_bytes!("../ferris.png")),
-        image::ImageFormat::Png,
+    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(
+        &display,
+        glium::index::PrimitiveType::TrianglesList,
+        &teapot::INDICES,
     )
-    .unwrap()
-    .to_rgba8();
-    let image_dimensions = image.dimensions();
-    let image =
-        glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-    let texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
-
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-        tex_coords: [f32; 2],
-    }
-
-    implement_vertex!(Vertex, position, tex_coords);
-
-    let vertex1 = Vertex {
-        position: [-0.5, -0.5],
-        tex_coords: [0., 0.],
-    };
-
-    let vertex2 = Vertex {
-        position: [0.5, -0.25],
-        tex_coords: [0., 1.],
-    };
-
-    let vertex3 = Vertex {
-        position: [0., 0.5],
-        tex_coords: [1., 0.],
-    };
-
-    let shape = vec![vertex1, vertex2, vertex3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    .unwrap();
 
     let vertex_shader_src = r#"
     #version 140
 
-    in vec2 position;
-    in vec2 tex_coords;
-    out vec2 v_tex_coords;
+    in vec3 position;
+    in vec3 normal;
 
     uniform mat4 matrix;
 
     void main() {
-        v_tex_coords = tex_coords;
-        gl_Position = matrix * vec4(position, 0.0, 1.0 );
+        gl_Position = matrix * vec4(position, 1.0 );
     }
     "#;
 
     let fragment_shader_src = r#"
     #version 140
 
-    in vec2 v_tex_coords;
     out vec4 color;
 
-    uniform sampler2D tex;
-
     void main() {
-        color = texture(tex, v_tex_coords);
+        color = vec4(1.0, 0.0, 0.0, 1.0);
     }
     "#;
 
@@ -85,8 +50,11 @@ fn main() {
         glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
             .unwrap();
 
-    let mut t: f32 = -0.5;
     events_loop.run(move |event, _, control_flow| {
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
@@ -103,34 +71,21 @@ fn main() {
             _ => return,
         }
 
-        t += 0.002;
-        if t > 0.5 {
-            t = -0.5;
-        }
-
-        // Setting the event loop to 60 FPS fallback
-        let next_frame_time =
-            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
-        let uniforms = uniform! {
-            matrix: [
-                [ t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0f32],
-            ],
-            tex: &texture,
-        };
+        let matrix = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 0.0, 1.0f32],
+        ];
 
         let mut target = display.draw();
-        target.clear_color(0., 0., 1., 1.);
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
         target
             .draw(
-                &vertex_buffer,
+                (&positions, &normals),
                 &indices,
                 &program,
-                &uniforms,
+                &uniform! { matrix: matrix },
                 &Default::default(),
             )
             .unwrap();
