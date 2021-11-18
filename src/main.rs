b75895913aaeb2bd6a340f0c98cdb1363a083c94
Benoit Delemps <benoitdelemps@gmail.com>
2021-11-18 14:34:08 +0100

    depth buffer

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     let events_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &events_loop).expect("display building failed");
 
     let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
@@ -28,10 +28,12 @@ fn main() {
 
     in vec3 position;
     in vec3 normal;
+    out vec3 v_normal;
 
     uniform mat4 matrix;
 
     void main() {
+        v_normal = transpose(inverse(mat3(matrix))) * normal;
         gl_Position = matrix * vec4(position, 1.0 );
     }
     "#;
@@ -39,10 +41,15 @@ fn main() {
     let fragment_shader_src = r#"
     #version 140
 
+    in vec3 v_normal;
     out vec4 color;
+    uniform vec3 u_light;
 
     void main() {
-        color = vec4(1.0, 0.0, 0.0, 1.0);
+        float brightness = dot(normalize(v_normal), normalize(u_light));
+        vec3 dark_color = vec3(0.6, 0.0, 0.0);
+        vec3 regular_color = vec3(1.0, 0.0, 0.0);
+        color = vec4(mix(dark_color,regular_color,brightness), 1.0);
     }
     "#;
 
@@ -78,15 +85,28 @@ fn main() {
             [0.0, 0.0, 0.0, 1.0f32],
         ];
 
+        // the direction of the light
+        let light = [-1.0, 0.4, 0.9f32];
+
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess,
+                write: true,
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
         target
             .draw(
                 (&positions, &normals),
                 &indices,
                 &program,
-                &uniform! { matrix: matrix },
-                &Default::default(),
+                &uniform! { matrix: matrix, u_light: light },
+                &params,
             )
             .unwrap();
 
